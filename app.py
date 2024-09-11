a08c20e0de80f15119ade7d48dfb8efbbac2a683
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-09-11 19:32:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import streamlit as st
 import pandas as pd
 
 # Replace 'your_file.csv' with the path to your CSV file
 df = pd.read_csv('database - Sheet 1.csv')
 
 # Display the DataFrame
-print(df)
+st.write(df)
